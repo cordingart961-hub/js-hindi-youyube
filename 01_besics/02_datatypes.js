ecab592da92a59d9c13cf6afda2d36b42ec8456f
ANOOP PATEL <cordingart961@gmail.com>
2025-10-17 17:06:22 +0000

    done

--- 01_besics/02_datatypes.js
+++ 01_besics/02_datatypes.js
@@ -1,1 +1,29 @@
+ "use strict"; // treat all js code as newer version
  
+ //alert( 3 + 3); // we are using node.js, not browser
+
+ console.log( 3 +3 )
+ // code readability should be high
+ console.log('anoop')
+
+  let name = 'anoop';
+  let age = 21;
+  let isLoggedIn = false
+
+    // primetive datatype
+
+  // number => 2 to power 53
+  // bigint
+  // string => ""
+  // boolean => true/false
+  // null => standalone value
+  // undefined =>
+  // symbol => unique
+
+
+  // noneprimitive datatypes
+
+   // object
+
+   console.log(typeof undefined) //undefined
+   console.log(typeof null) // object
